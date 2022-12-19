5996aaffc93e674575d828132f30151815313e0d
andrew glass <andrew.glass@outlook.com>
2022-12-19 13:38:54 +0000

    resolved attaching route table and igw to subnet

--- route_table.tf
+++ route_table.tf
@@ -2,16 +2,11 @@ resource "aws_route_table" "openai_rtb" {
   vpc_id = aws_vpc.openai_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.openai_igw.id
   }
 
   tags = {
     Name = "openai_rtb"
   }
-}
-
-resource "aws_route_table_association" "openai_rtb_a" {
-  subnet_id      = aws_subnet.openai_subnet.id
-  route_table_id = "openai_rtb"
 }
